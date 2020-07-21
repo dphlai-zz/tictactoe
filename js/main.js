ecedb367fdc41c33e2020d6a327919fc7d228216
Danny Lai <dannyphlai@gmail.com>
2020-07-21 11:09:17 +1000

    added placeMark() isPlayerOne()

--- js/main.js
+++ js/main.js
@@ -1,1 +1,39 @@
-console.log('Tic Tac Toe!');
+console.log('Tic Tac Toe');
+
+let board = [
+  '', '', '',
+  '', '', '',
+  '', '', '',
+];
+
+let turn = 1;
+
+const winLogic = function() {
+}; //end of winLogic
+
+const isPlayerOne = function() {
+  turn += 1;
+  return turn % 2 === 0
+}; //end of playerTurn
+
+const placeMark = function(event) {
+  const index = parseInt(event.currentTarget.id) - 1;
+  let markValue = '';
+  if(board[index] == ''){
+    if(isPlayerOne()){
+      markValue = 'X';
+    } else {
+      markValue = 'O';
+    };
+    board[index] = markValue;
+    $(event.currentTarget).text(markValue);
+  } else {
+    alert(`This square is already occupied by a mark.`);
+  }
+}; //end of placeMark
+
+$(document).ready(function() {
+
+  $('.box').on('click', placeMark); //end of .box click handler
+
+}); // $(document).ready()
